07183e529dd13291e8d9cd5befbd472d558fa31f
nassan <nassanpaul@gmail.com>
2025-01-03 15:18:22 +0200

    fix: Temp revert to iframe

--- vite-wc/src/lib/ContentYoutube.svelte
+++ vite-wc/src/lib/ContentYoutube.svelte
@@ -2,26 +2,26 @@
   import { onDestroy, onMount } from "svelte";
   let { sref } = $props();
 
-  let uid = null
+  let uid = null;
 
-  onMount(()=>{
-    console.log('content plugin onMount called')
-  })
+  onMount(() => {
+    console.log("content plugin onMount called");
+  });
 
-  onDestroy(()=>{
-    console.log('content plugin onDestroy called')
-    const elem = document.getElementById(uid)
-    if(elem){
-      elem.remove()
+  onDestroy(() => {
+    console.log("content plugin onDestroy called");
+    const elem = document.getElementById(uid);
+    if (elem) {
+      elem.remove();
     }
-  })
+  });
 
-  function CustomDiv(node){
+  function CustomDiv(node) {
     uid = crypto.randomUUID();
-    const childDivElem = document.createElement('div');
+    const childDivElem = document.createElement("div");
     childDivElem.id = uid;
-    node.appendChild(childDivElem)
-    console.log('before player instantiation', node.children[0])
+    node.appendChild(childDivElem);
+    console.log("before player instantiation", node.children[0]);
     // @ts-ignore
     const player = new YT.Player(uid, {
       height: "315",
@@ -30,8 +30,18 @@
       autoplay: "1",
       rel: "0",
     });
-    console.log('after player instantiation', node.children[0])
+    console.log("after player instantiation", node.children[0]);
   }
 </script>
 
-<div class="yt-wrapper" use:CustomDiv></div>
+<!-- <div class="yt-wrapper" use:CustomDiv></div> -->
+<iframe
+  frameborder="0"
+  allowfullscreen
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  referrerpolicy="strict-origin-when-cross-origin"
+  title="Introduction to the Zohar by Rabbi Ashlag.wmv"
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/2ixetloUNE4?enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost%3A5173&amp;widgetid=1"
+></iframe>
