8e4f932479411fea3576051dbb0fd19e08a4a281
nassan <nassanpaul@gmail.com>
2025-01-05 00:36:41 +0200

    chore: remove dependency on svelte use action

--- vite-wc/src/lib/ContentYoutube.svelte
+++ vite-wc/src/lib/ContentYoutube.svelte
@@ -1,29 +1,22 @@
 <script>
-  import { onDestroy, onMount } from "svelte";
+  import { onMount } from "svelte";
   import YouTubePlayer from "youtube-player";
 
   let { sref } = $props();
 
-  let uid = null;
+  const uid = crypto.randomUUID();
+  let player = null;
 
-  function CustomDiv(node) {
-    uid = crypto.randomUUID();
-    const childDivElem = document.createElement("div");
-    childDivElem.id = uid;
-    node.appendChild(childDivElem);
-    console.log("before player instantiation", node.children[0]);
-    const player = YouTubePlayer(uid, {
+  onMount(()=>{
+    player = YouTubePlayer(uid, {
       width: "100%",
       videoId: "2ixetloUNE4",
       autoplay: "1",
       rel: "0",
     });
-    console.log("after player instantiation", node.children[0]);
-
-    setTimeout(() => {
-      console.log("after a while", node.children[0]);
-    }, 1000);
-  }
+  })
 </script>
 
-<div class="yt-wrapper" use:CustomDiv></div>
+<div class="yt-wrapper">
+  <div id={uid}></div>
+</div>
